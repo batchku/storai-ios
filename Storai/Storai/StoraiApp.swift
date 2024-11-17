1a6cef7151648972cbcaee9a6cdcecc7fdf376fd
Ali Momeni <batchku@gmail.com>
2024-11-17 12:36:42 -0800

    initial push of iOS xcode project

---
+++ Storai/Storai/StoraiApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoraiApp.swift
+//  Storai
+//
+//  Created by Ali Momeni on 11/17/24.
+//
+
+import SwiftUI
+
+@main
+struct StoraiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
